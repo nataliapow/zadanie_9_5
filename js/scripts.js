58033cb33e0340e3a09025dc5eace5ec09f2e7e2
Natalia <n.powalska@gmail.com>
2017-08-27 20:48:08 +0200

    Change content inside the loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,7 @@
 var withButtonClass = document.getElementsByClassName('button'),
-ourTable = ['button', 'button', 'button', 'button'],
 i;
 
-console.log(ourTable.length);
-
-for (i=0; i<ourTable.length; i++)  {
-	withButtonClass.innerText = 'Hello!';
-	withButtonClass.innerText = 'How';
-	withButtonClass.innerText = 'are';
-	withButtonClass.innerText = 'you?';
-
+for (i=0; i<withButtonClass.length; i++)  {
+	withButtonClass[i].innerText;
+	console.log(withButtonClass[i].innerText);
 }
