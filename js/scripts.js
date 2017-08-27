4bda4a48411ebb598927a486d7050112bea3acd9
Natalia <n.powalska@gmail.com>
2017-08-27 12:24:06 +0200

    Innitial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var withButtonClass = document.getElementsByClassName('button'),
+ourTable = ['button', 'button', 'button', 'button'],
+i;
+
+console.log(ourTable.length);
+
+for (i=0; i<ourTable.length; i++)  {
+	withButtonClass.innerText = 'Hello!';
+	withButtonClass.innerText = 'How';
+	withButtonClass.innerText = 'are';
+	withButtonClass.innerText = 'you?';
+
+}
