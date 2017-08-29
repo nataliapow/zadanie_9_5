e62aae1a09f0901aaf31bde5ca266a2ac070b32c
Natalia <n.powalska@gmail.com>
2017-08-29 21:45:02 +0200

    Remove unnecessary code

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,5 @@ var withButtonClass = document.getElementsByClassName('button'),
 i;
 
 for (i=0; i<withButtonClass.length; i++)  {
-	withButtonClass[i].innerText;
 	console.log(withButtonClass[i].innerText);
 }
